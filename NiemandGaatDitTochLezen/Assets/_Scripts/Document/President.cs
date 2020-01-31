a5684d76e4f4da2f506b41047787aa459e4224b3
Moomnies <Vera.dawn94@gmail.com>
2020-01-31 23:52:56 +0100

    Made Countries and Presidents + HomeCountries

--- NiemandGaatDitTochLezen/Assets/_Scripts/Document/President.cs
+++ NiemandGaatDitTochLezen/Assets/_Scripts/Document/President.cs
@@ -7,9 +7,12 @@ public class President : ScriptableObject
 {
     public string getPresidentName { get { return presidentName; } private set { presidentName = value; } }
     public Sprite getPresidentPortrait { get { return presidentPortrait; } private set { presidentPortrait = value; } }
+    public Country getHomeCOuntry { get { return homeCountry; } private set { homeCountry = value; } }
 
     [SerializeField]
     string presidentName;
+    [SerializeField]
+    Country homeCountry;
     [SerializeField]
     Sprite presidentPortrait;
 }
