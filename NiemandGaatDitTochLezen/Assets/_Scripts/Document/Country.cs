e8738b3682c7671dfdf4db82523624e1bf86e12a
Moomnies <Vera.dawn94@gmail.com>
2020-01-31 22:48:45 +0100

    Document ScriptableObjects

---
+++ NiemandGaatDitTochLezen/Assets/_Scripts/Document/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCountry", menuName = "Document/Country")]
+public class Country : ScriptableObject
+{
+    public string getCountryName { get { return countryName; } private set { countryName = value; } }
+
+    [SerializeField]
+    string countryName;
+}
---
+++ NiemandGaatDitTochLezen/Assets/_Scripts/Document/President.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newPresident", menuName = "Document/President")]
+public class President : ScriptableObject
+{
+    public string getPresidentName { get { return presidentName; } private set { presidentName = value; } }
+    public Sprite getPresidentPortrait { get { return presidentPortrait; } private set { presidentPortrait = value; } }
+
+    [SerializeField]
+    string presidentName;
+    [SerializeField]
+    Sprite presidentPortrait;
+}
