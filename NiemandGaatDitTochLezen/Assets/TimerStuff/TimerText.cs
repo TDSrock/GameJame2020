aec24e2b3c2975382a91f5635edceca41744eb5d
Tim De Graaff <timdegraaff1995@gmail.com>
2020-01-31 23:58:41 +0100

    Timer implemented

---
+++ NiemandGaatDitTochLezen/Assets/TimerStuff/TimerText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerText : MonoBehaviour
+{
+    public float timer;
+    public TextMeshProUGUI startText;
+    private bool timeLeft = true;
+
+    private void Start() {
+       string minutes = Mathf.Floor(timer / 60).ToString("00");
+       string seconds = (timer % 60).ToString("00");
+    }
+
+    void Update() {
+        
+        if (timeLeft) {
+            if (timer < 1) {
+                timeLeft = false;
+                //FANCY EVENT DIE ER NOG IN MOET
+            }
+            timer -= Time.deltaTime;
+            string minutes = Mathf.Floor(timer / 60).ToString("00");
+            string seconds = Mathf.Floor(timer % 60).ToString("00");
+            startText.text = minutes + ":" + seconds;
+            
+        }
+        
+      
+    }
+}
