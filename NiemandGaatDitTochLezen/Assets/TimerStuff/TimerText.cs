94be376edeb31410e64a24ee239a17f0fa8a4318
Tim De Graaff <timdegraaff1995@gmail.com>
2020-02-01 02:28:05 +0100

    added sound file for the timer

--- NiemandGaatDitTochLezen/Assets/TimerStuff/TimerText.cs
+++ NiemandGaatDitTochLezen/Assets/TimerStuff/TimerText.cs
@@ -7,27 +7,37 @@ public class TimerText : MonoBehaviour
 {
     public float timer;
     public TextMeshProUGUI startText;
+    public AudioSource clickSound;
     private bool timeLeft = true;
 
     private void Start() {
        string minutes = Mathf.Floor(timer / 60).ToString("00");
        string seconds = (timer % 60).ToString("00");
+       StartCoroutine(ChangeTime());
     }
 
-    void Update() {
-        
+    void FixedUpdate() {
+        timer -= Time.deltaTime;
         if (timeLeft) {
+            string minutes = Mathf.Floor(timer / 60).ToString("00");
+            string seconds = Mathf.Floor(timer % 60).ToString("00");
+            startText.text = minutes + ":" + seconds;
             if (timer < 1) {
                 timeLeft = false;
                 //FANCY EVENT DIE ER NOG IN MOET
             }
-            timer -= Time.deltaTime;
-            string minutes = Mathf.Floor(timer / 60).ToString("00");
-            string seconds = Mathf.Floor(timer % 60).ToString("00");
-            startText.text = minutes + ":" + seconds;
-            
         }
         
-      
+
     }
+
+    IEnumerator ChangeTime() {
+
+        while (timeLeft) {
+            clickSound.Play();
+            yield return new WaitForSecondsRealtime(1);
+            
+        }
+    }
+
 }
