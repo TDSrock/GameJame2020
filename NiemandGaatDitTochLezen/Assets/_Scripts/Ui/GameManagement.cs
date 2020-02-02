d72c5be7dd63483cbff0725274d2dbd7bd3aa81d
TDSrock <dhr.sjors.gielen@gmail.com>
2020-02-02 04:23:17 +0100

    Don't mind me, going to master

---
+++ NiemandGaatDitTochLezen/Assets/_Scripts/Ui/GameManagement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagement : MonoBehaviour
+{
+    static GameManagement instance;
+    public static GameManagement Instance
+    {
+        get {
+            if(instance == null)
+            {
+                Debug.LogError("Some bitch forgot to actually put in the GameManagement object.");
+            }
+            return instance; }
+    }
+
+    [Header("Document related shit")]
+    GameObject NoRelevantItemsFoundUI;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void OpenDocuemnt(Document doc)
+    {
+        //fuck up the state a bit
+
+        //do doc logic
+        if(doc == null)
+        {
+            NoRelevantItemsFoundUI.SetActive(true);
+        }
+        //now the fun shit comes
+    }
+
+    public void CloseDocument()
+    {
+        //restore state
+    }
+}
