c71893cf9cc8f5185cd058ba15d96d32f60f5d2a
TDSrock <dhr.sjors.gielen@gmail.com>
2020-02-02 03:24:44 +0100

    DESK IS DONE FML

--- NiemandGaatDitTochLezen/Assets/_Scripts/FrontViewObjects/FrontView.cs
+++ NiemandGaatDitTochLezen/Assets/_Scripts/FrontViewObjects/FrontView.cs
@@ -51,7 +51,8 @@ public class FrontView : MonoBehaviour, IInteractable
     {
         foreach(var col in interactionColliders)
         {
-            col.enabled = false;
+            if(col.isTrigger)
+                col.enabled = false;
         }
         StopAllCoroutines();
         StartCoroutine(MoveCamera(frontViewCameraLocation.transform.position, frontViewCameraLocation.transform.rotation));
@@ -108,7 +109,8 @@ public class FrontView : MonoBehaviour, IInteractable
     {
         foreach (var col in interactionColliders)
         {
-            col.enabled = true;
+            if (col.isTrigger)
+                col.enabled = true;
         }
         frontViewInteractor.enabled = false;
         StopAllCoroutines();
